3a6f1fc2a13d33498487e9ee184b3f45603ff640
Protsiuk-Prits Dmytro <78557776+ProtsyukPrits@users.noreply.github.com>
2022-10-16 18:03:15 +0200

    hook fetch, loadmore...

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,6 @@ import { useState, useEffect } from 'react';
 import { ImageGallery } from './ImageGallery';
 import { Searchbar } from './Searchbar';
 import { addBaseFetch } from '..//services/api';
-//
 import { Container } from './Common.styled';
 import { Button } from './Button';
 import { Modal } from './Modal';
@@ -22,13 +21,15 @@ export const App = () => {
   const [totalPages, setTotalPages] = useState(null);
 
   useEffect(() => {
+    if (!query) {
+      return;
+    }
     try {
       const dataFetch = async () => {
         setIsLoading(true);
 
         const data = await addBaseFetch(query, page, perPage);
         setMaterials(materials => [...materials, ...data.hits]);
-        console.log('dataFetch', data.hits);
 
         setIsLoading(false);
       };
@@ -37,8 +38,6 @@ export const App = () => {
       setError({ error });
     }
   }, [page, perPage, query]);
-
-  console.log('render', materials);
 
   const handleSubmit = e => {
     e.preventDefault();
@@ -99,129 +98,8 @@ export const App = () => {
       {/* LoadMoreBTN */}
       {page === totalPages
         ? !isLoading && <p>End of content</p>
-        : materials.length > 0 && (
-            <Button loadMore={handleLoadMore} isLoading={isLoading} />
-          )}
+        : materials.length > 0 && <Button loadMore={handleLoadMore} />}
     </Container>
   );
 };
 
-// export class App extends Component {
-//   state = {
-//     materials: [],
-//     isLoading: false,
-//     error: null,
-//     showModal: false,
-//     query: '',
-//     page: 1,
-//     photoIsActive: null,
-//     perPage: 12,
-//     totalPages: null,
-//   };
-
-//   async componentDidUpdate(_, prevState) {
-//     const { query, page, perPage } = this.state;
-//     try {
-//       if (prevState.page !== page || prevState.query !== query) {
-//         this.setState({ isLoading: true });
-//         const dataFetch = await addBaseFetch(query, page, perPage);
-//         this.setState(prevState => ({
-//           materials: [...prevState.materials, ...dataFetch.hits],
-//           isLoading: false,
-//         }));
-//       }
-//     } catch (error) {
-//       this.setState({ error });
-//     }
-//   }
-
-//   handleSubmit = e => {
-//     e.preventDefault();
-
-//     if (this.state.query.trim() === '') {
-//       toast.error('Nothing found, enter something in the search');
-//     }
-
-//     this.setState({
-//       page: 1,
-//       query: e.target.elements.query.value,
-//       materials: [],
-//       totalPages: null,
-//     });
-//     e.target.reset();
-//   };
-
-//   handleLoadMore = async e => {
-//     const { query, perPage } = this.state;
-//     //
-//     const loadMoreDataFetch = await addBaseFetch(query);
-//     const totalPages = Math.ceil(loadMoreDataFetch.total / perPage);
-//     this.setState({ totalPages: totalPages });
-//     // ------
-
-//     this.setState(prevState => ({
-//       page: prevState.page + 1,
-//     }));
-//   };
-
-//   openModal = e => {
-//     const imageId = Number(e.currentTarget.id);
-//     const photoIsActive = this.state.materials.find(
-//       image => image.id === imageId
-//     );
-//     this.setState({
-//       photoIsActive,
-//       showModal: true,
-//     });
-//   };
-
-//   closeModal = () => {
-//     this.setState({ showModal: false });
-//   };
-
-//   render() {
-//     const {
-//       query,
-//       materials,
-//       page,
-//       totalPages,
-//       error,
-//       showModal,
-//       photoIsActive,
-//       isLoading,
-//     } = this.state;
-
-//     return (
-//       <Container>
-//         {showModal && (
-//           <Modal onClose={this.closeModal}>
-//             {/* Я вирішив зробити модалку як універсальнна обгортка,
-//              а в середині рендерити контент який потрібно. */}
-//             <img src={photoIsActive.largeImageURL} alt={photoIsActive.tags} />
-//           </Modal>
-//         )}
-//         {/* Input */}
-//         <Searchbar onSubmit={this.handleSubmit} />
-//         {materials.length === 0 && !isLoading && (
-//           <p>Please, enter something in the search</p>
-//         )}
-//         {/*Toast  */}
-//         {query === '' && <ToastContainer />}
-//         {/* Gallery */}
-//         {error && <p>Wooops some errors!!!</p>}
-//         {materials.length > 0 && (
-//           <ImageGallery materials={materials} onClick={this.openModal} />
-//         )}
-//         {/* Loader */}
-//         {isLoading && <Loader />}
-
-//         {/* LoadMoreBTN */}
-//         {page === totalPages
-//           ? !isLoading && <p>End of content</p>
-//           : materials.length > 0 && (
-//               <Button loadMore={this.handleLoadMore} isLoading={isLoading} />
-//             )}
-//       </Container>
-//     );
-//   }
-// }
